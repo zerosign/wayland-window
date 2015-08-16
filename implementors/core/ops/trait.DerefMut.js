92793cf3fb11d9539a9417a7a8a1175ee19b27be
Victor Berger <victor.berger@m4x.org>
2015-08-16 11:14:00 +0200

    Update docs for 2488b4134757e5e4e4d169f4e6acd41faa19ed22
    Uniformize i32 for dimensions.

--- implementors/core/ops/trait.DerefMut.js
+++ implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['wayland_window'] = [];
+implementors['wayland_window'] = ["impl&lt;'a, S: <a class='trait' href='wayland_client/core/trait.Surface.html' title='wayland_client::core::Surface'>Surface</a> + 'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='wayland_window/struct.SurfaceGuard.html' title='wayland_window::SurfaceGuard'>SurfaceGuard</a>&lt;'a, S&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
