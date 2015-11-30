acd0e62fb98a4e54a08a5e19e7a8078a83cddd8b
Victor Berger <victor.berger@m4x.org>
2015-11-30 16:19:29 +0100

    Update docs for 8cf2d40481423aef84f87d9c3d316f95ec8db742
    Merge pull request #2 from vberger/vberger
    
    Update lib to wayland-client-0.4

--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['wayland_window'] = [];
+implementors['wayland_window'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='wayland_window/struct.DecoratedSurface.html' title='wayland_window::DecoratedSurface'>DecoratedSurface</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
